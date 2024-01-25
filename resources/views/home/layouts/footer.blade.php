7db1ea4046df8f0f9432639e5ad35272ed38ada0
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2024-01-25 19:17:25 +0700

    Home (#2)
    * create landing page
    
    * fix admin dashboard, comment out some JS lib
    
    * comment out unused JS lib

---
+++ resources/views/home/layouts/footer.blade.php
@@ -0,0 +1,45 @@
+<footer class="footer py-6 bg-gray-800 text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="navbar-brand-dark mb-4" height="35" src="/assets/img/light.svg" alt="Logo light">
+                <p>Volt is a Premium Bootstrap 5 Admin Dashboard bringing together beautiful UI/UX design and functional elements.</p>
+            </div>
+            <div class="col-6 col-md-2 mb-5 mb-lg-0">
+                <span class="h5">Other</span>
+                <ul class="links-vertical mt-2">
+                    <li><a href="#" target="_blank">Changelog</a></li>
+                    <li><a target="_blank" href="#">License</a></li>
+                    <li><a target="_blank" href="#">Support</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4 mb-5 mb-lg-0">
+                <span class="h5 mb-3 d-block">Subscribe</span>
+                <form action="#">
+                    <div class="form-row mb-2">
+                        <div class="col-12">
+                            <input type="email" class="form-control mb-2" placeholder="example@company.com" name="email" aria-label="Subscribe form" required>
+                        </div>
+                        <div class="col-12 d-grid">
+                            <button type="submit" class="btn btn-secondary" data-loading-text="Sending">
+                                <span>Subscribe</span>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <p class="text-muted font-small m-0">We’ll never share your details. See our <a class="text-white" href="#">Privacy Policy</a></p>
+            </div>
+        </div>
+        <hr class="bg-gray-700 my-5">
+        <div class="row">
+            <div class="col mb-md-0">
+                <a href="#" target="_blank" class="d-flex justify-content-center">
+                    <img src="/assets/img/light.svg" height="35" class="mb-4" alt="Themesberg Logo">
+                </a>
+            <div class="d-flex text-center justify-content-center align-items-center" role="contentinfo">
+                <p class="fw-normal font-small mb-0">Copyright © Ahmad Ma'ruf 2024. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
