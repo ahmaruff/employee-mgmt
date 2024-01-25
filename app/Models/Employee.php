6c682f39eff24119d8e1cf4578f421a3a4f98a0f
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2024-01-25 19:38:32 +0700

    add Salary Model, migration and relationship with Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
 {
@@ -25,5 +26,10 @@ class Employee extends Model
     public function division() : BelongsTo
     {
         return $this->belongsTo(Division::class);
+    }
+
+    public function salary() : HasOne
+    {
+        return $this->hasOne(Salary::class);
     }
 }
