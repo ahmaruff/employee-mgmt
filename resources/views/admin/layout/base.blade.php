45f316170c3d48b460586346db02e5e2d0eed7ad
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2024-01-25 19:10:39 +0700

    fix admin dashboard, comment out some JS lib

--- resources/views/admin/layout/base.blade.php
+++ resources/views/admin/layout/base.blade.php
@@ -47,16 +47,16 @@
 <script src="/assets/vendor/@popperjs/core/dist/umd/popper.min.js"></script>
 <script src="/assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 
-<script src="/assets/vendor/onscreen/dist/on-screen.umd.min.js"></script>
-<script src="/assets/vendor/nouislider/distribute/nouislider.min.js"></script>
+{{-- <script src="/assets/vendor/onscreen/dist/on-screen.umd.min.js"></script> --}}
+{{-- <script src="/assets/vendor/nouislider/distribute/nouislider.min.js"></script> --}}
 <script src="/assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
-<script src="/assets/vendor/chartist/dist/chartist.min.js"></script>
-<script src="/assets/vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+{{-- <script src="/assets/vendor/chartist/dist/chartist.min.js"></script> --}}
+{{-- <script src="/assets/vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script> --}}
 <script src="/assets/vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
 <script src="/assets/vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script> --}}
 <script src="/assets/vendor/notyf/notyf.min.js"></script>
-<script src="/assets/vendor/simplebar/dist/simplebar.min.js"></script>
+{{-- <script src="/assets/vendor/simplebar/dist/simplebar.min.js"></script> --}}
 <script src="/assets/js/volt.js"></script>
 </body>
 </html>
