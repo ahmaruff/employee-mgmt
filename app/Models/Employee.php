154583009b77de5c304538846356a81d8cb8311f
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2024-01-25 20:00:32 +0700

    add position model, migration, and pivot table for many-to-many with employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
@@ -31,5 +32,10 @@ class Employee extends Model
     public function salary() : HasOne
     {
         return $this->hasOne(Salary::class);
+    }
+
+    public function positions() : BelongsToMany
+    {
+        return $this->belongsToMany(Position::class);
     }
 }
